f0ba121cc75879c7dd0790f70ae77e05a4f710a2
Seif <seif.dorani@gmail.com>
2017-01-19 09:42:02 -0500

    planning complete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
 //Add new Task
   //When button is pressed
-  //Create a task
+  //Create a new list item with the text from new-task:
+  //input (checkbox)
+  //label
+  //input (text)
+  //button.edit
+  //button.delete
+  //each elements, needs to be modified and appended
 
 //Edit existing tasks
+  //When the Edit button is pressed
+   //if the class of parent is edit mode
+   //switch from editmode
+   //label text become inputs value
+ //else
+  //switch to edit mode
+  //input value becomes the labels text
+
+ //toggle edit mode on parent
 
 //Delete an existing task
+  //when delete button is pressed
+   //remove parent list item from ul
 
 //Mark a task as completed-tasks
+  //when the checkbox is checked
+    //append the tast list item to the #completed-tasks
 
 //Mark a task as incomplete-tasks
+  //when the checkbox is unchecked we want to append it to #incomplete-tasks
